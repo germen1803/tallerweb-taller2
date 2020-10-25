e1f6e71c342e2308915ab2af1ebeb57b762e68cf
germen1803 <4germen1803@users.noreply.github.com>
2020-10-25 03:21:23 -0300

    version 2

---
+++ routes/personaRoute.js
@@ -0,0 +1,24 @@
+'use strict'
+ 
+// Cargamos el módulo de express para poder crear rutas
+var express = require('express');
+ 
+// Cargamos el controlador
+var personaController = require('../controllers/personaController');
+// var autoController = require('../controllers/autoController');
+
+// Llamamos al router
+var api = express.Router();
+ 
+//  Guardar Persona
+api.post('/personas', personaController.guardar);
+api.get('/personas', personaController.todos);
+api.get('/personabyID/:id', personaController.buscarPorID);
+api.delete('/personabyID/:id' , personaController.delet);
+api.put('/personabyID/:id' , personaController.put);
+
+
+// api.post('/autoguardar',autoController.guardar);
+
+// Exportamos la confi,guración
+module.exports = api;
